b1b422ed714bf8a805f040047ea24adec2b9bb5b
jhon.carmona <jhonedier.c@gmail.com>
2022-01-07 21:37:19 -0500

    Se excluye parquete TRM

---
+++ microservicio/infraestructura/src/test/java/com/ceiba/trm/controlador/ConsultaControladorTrmTest.java
@@ -0,0 +1,35 @@
+package com.ceiba.trm.controlador;
+
+import com.ceiba.ApplicationMock;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(ConsultaControladorTrm.class)
+@ContextConfiguration(classes= ApplicationMock.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class ConsultaControladorTrmTest {
+
+    @Autowired
+    private MockMvc mocMvc;
+
+    @Test
+    @DisplayName("Deberia consultar TRM")
+    void deberiConsultarTrm() throws Exception {
+        // arrange
+        // act - assert
+        mocMvc.perform(get("/trm"))
+                .andExpect(status().isOk())
+                .andReturn();
+        }
+}
+
